ceb757a9fea0e30e7ee0041463d8faf71ad916cb
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-02-23 17:54:09 -0300

    class COntroller generated automatically with build_runner

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,16 +1,14 @@
 import 'package:mobx/mobx.dart';
+part 'controller.g.dart';
 
-class Controller {
-  var _counter = Observable(0);
-  Action increment;
-  int get counter => _counter.value;
-  set counter(int newValue) => _counter.value = newValue;
+class Controller = ControllerBase with _$Controller;
 
-  Controller() {
-    increment = Action(_increment);
-  }
+abstract class ControllerBase with Store {
+  @observable
+  int counter = 0;
 
-  _increment() {
+  @action
+  increment() {
     counter++;
   }
 }
