8288455c10d3596d67c7f444b52e1061542aa8be
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-02-23 18:09:34 -0300

    home.dart changed to exercise name and last name

--- lib/home.dart
+++ lib/home.dart
@@ -12,10 +12,28 @@ class MyHomePage extends StatelessWidget {
         title: Text('Exercise Mobx'),
       ),
       body: Center(
-        child: Observer(
-          builder: (_) {
-            return Text('${controller.counter}');
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Sobrenome'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Observer(
+              builder: (_) {
+                return Text('${controller.counter}');
+              },
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
